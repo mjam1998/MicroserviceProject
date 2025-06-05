5c8e70155b6e0669c47499af952e602027fc7b46
mjam1998 <m.j.asadi.m@gmail.com>
2025-06-06 00:35:08 +0330

    cqrs handler start

---
+++ Services/Ordering/Ordering.Application/features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿
+
+using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public CheckoutOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+           var orderEntity=_mapper.Map<Order>(request);
+            var newOrder=await _orderRepository.AddAsync(orderEntity);
+            return newOrder.Id;
+        }
+    }
+}
